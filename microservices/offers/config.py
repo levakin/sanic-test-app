faf478cdc1db6e1ea067c54cf138f8f40d753a16
levakin <levakin@protonmail.com>
2018-07-18 23:04:42 +0500

    compose and port fix

--- microservices/offers/config.py
+++ microservices/offers/config.py
@@ -5,12 +5,12 @@ from distutils.util import strtobool
 
 
 class Config:
-    DEBUG = bool(strtobool(os.getenv('DEBUG', "False")))
+    DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
     DATABASE_URI = os.getenv('DATABASE_URI', '127.0.0.1:27017')
     WORKERS = int(os.getenv('WORKERS', 2))
     LOGO = os.getenv('LOGO', None)
     HOST = os.getenv('HOST', '127.0.0.1')
-    PORT = int(os.getenv('PORT', 8000))
+    PORT = int(os.getenv('PORT', 8001))
     SECRET = os.getenv('SECRET', 'secret')
     MAX_TITLE_LEN = int(os.getenv('MAX_TITLE_LEN', 30))
     MAX_TEXT_LEN = int(os.getenv('MAX_TEXT_LEN', 80))
--- microservices/offers/offers.py
+++ microservices/offers/offers.py
@@ -5,6 +5,7 @@ from config import Config
 
 if __name__ == "__main__":
     app = get_app()
+    print(Config.PORT)
     app.run(host=Config.HOST,
             port=Config.PORT,
             debug=Config.DEBUG,
